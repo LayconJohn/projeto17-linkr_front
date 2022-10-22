0c053348a2758eca69a03e6cd74754676c474972
Jansenck <caik11@hotmail.com>
2022-10-21 21:19:53 -0300

    were implemented on publish route a generic layout.

--- projeto17-linkr_front/src/components/App.js
+++ projeto17-linkr_front/src/components/App.js
@@ -1,12 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { Test } from "./elements/Test";
+import Timeline from "./elements/Timeline";
+
+import "../styles/reset.css";
+import "../styles/style.css";
 
 function App() {
     return (
         <>
             <BrowserRouter>
             <Routes>
+                <Route path="/publish" element={<Timeline/>} />
                 <Route path="/" element={<Test />} />
             </Routes>
             </BrowserRouter>
@@ -14,4 +19,4 @@ function App() {
     )
 }
 
-export {App};
+export { App };
